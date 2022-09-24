ac83dad4fa774cc05c1444665b072b650e9fd3a5
Luis Cascante <luisalonzocascante@gmail.com>
2022-09-24 09:11:54 -0600

    promise.all

--- Clase1/clase1/src/App.js
+++ Clase1/clase1/src/App.js
@@ -1,47 +1,9 @@
-import Prueba from "./Prueba";
-import { PruebaFuncion } from "./PruebaFuncion";
+import { ListaPublicaciones } from "./components/organisms/ListaPublicaciones";
 
 function App() {
-  const nombre = "Juan";
-  const fechaActual = new Date();
-  const listaEstudiantes = [
-    {
-      nombre: "Juan",
-      edad: 20,
-    },
-    {
-      nombre: "Pedro",
-      edad: 30,
-    },
-    {
-      nombre: "Maria",
-      edad: 40,
-    },
-  ];
-
-  const sacarNumeroAleatorio = () => {
-    return Math.random();
-  };
-
-  const manejarClick = () => {
-    console.log("Hola");
-  };
-
   return (
     <div>
-      <PruebaFuncion />
-      {/* Javascript no se queja por la prop */}
-      <Prueba apellido="Luis Cascante" />
-
-      <ul>
-        {listaEstudiantes.map((estudiante) => (
-          <li>
-            <p>Nombre: {estudiante.nombre}</p>
-            <p>Edad: {estudiante.edad}</p>
-          </li>
-        ))}
-      </ul>
-      <button onClick={manejarClick}>Click me!</button>
+      <ListaPublicaciones />
     </div>
   );
 }
